95c705319968f0fc0e25b89209d49d57b598fc27
JLashua9 <jlashua@uvm.edu>
2024-11-11 07:43:51 -0500

    88. Merge Sorted Array

---
+++ 876. MIddle_of_LinkedList.cpp
@@ -0,0 +1,21 @@
+//Jami Lashua
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
+#import <ListNode>
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *fast = head, *slow = head;
+        while (fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
